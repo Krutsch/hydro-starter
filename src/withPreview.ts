bef0e41ff4b905334614282870145228d7652b10
Fabian Krutsch <f.krutsch@gmx.de>
2020-12-09 21:53:29 +0000

    fix image preview build step

--- src/withPreview.ts
+++ src/withPreview.ts
@@ -1,6 +1,9 @@
 export default function withPreview(img: string) {
   if (import.meta.env.MODE === "development") return img;
 
-  const dist = "/_dist_/";
-  return `${dist}preview-${img.split(dist).pop()}`;
+  const pathArray = img.split("/");
+  pathArray[pathArray.length - 1] = `preview-${
+    pathArray[pathArray.length - 1]
+  }`;
+  return pathArray.join("/");
 }
