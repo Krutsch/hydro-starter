bf4a1d7167aff31023be2c8c672696ad20311098
Fabian Krutsch <f.krutsch@gmx.de>
2021-01-28 21:04:52 +0000

    refactor to use html-bundle

--- src/Link/index.ts
+++
@@ -1,7 +0,0 @@
-import { html } from "hydro-js";
-
-export function Link(): ReturnType<typeof html> {
-  return html`<a class="learn" href="https://github.com/Krutsch/hydro-js"
-    >Learn hydro-js</a
-  >`;
-}
