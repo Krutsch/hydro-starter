5ccea651cae9cf3a5620a198deee317e3d55924d
Fabian Krutsch <f.krutsch@gmx.de>
2021-01-30 16:59:50 +0000

    refactor web worker

---
+++ src/@types/index.d.ts
@@ -0,0 +1,3 @@
+export interface worker {
+  fib: (n: number) => Promise<number>;
+}
---
+++ src/workerCode.ts
@@ -0,0 +1,22 @@
+import { worker } from "./@types";
+
+// If module worker are supported in Firefox and Safari
+// import * as Comlink from "comlink";
+
+// Meanwhile
+importScripts("https://unpkg.com/comlink/dist/umd/comlink.min.js");
+
+const worker = {
+  fib(n: number) {
+    const table = Array(n + 1).fill(0);
+    table[1] = 1;
+    for (let i = 0; i <= n; i++) {
+      table[i + 1] += table[i];
+      table[i + 2] += table[i];
+    }
+    return table[n];
+  },
+} as worker;
+
+//@ts-ignore for now.
+Comlink.expose(worker);
