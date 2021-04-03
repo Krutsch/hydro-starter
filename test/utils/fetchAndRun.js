e05699e0c3fd7e0df171ff952a2a57277e506391
Fabian Krutsch <45216083+Krutsch@users.noreply.github.com>
2021-04-03 09:10:47 +0100

    fix: foldername

---
+++ test/utils/fetchAndRun.js
@@ -0,0 +1,17 @@
+import { html, render, setGlobalSchedule } from "hydro-js";
+import { runTests } from "@web/test-runner-mocha";
+setGlobalSchedule(false);
+
+/**
+ * @param {string} file
+ * @param {() => unknown} fn
+ */
+export default function fetchAndRun(file, fn) {
+  fetch(file)
+    .then((e) => e.text())
+    .then((t) => {
+      render(html`${t}`);
+      runTests(fn);
+    })
+    .catch((e) => console.error(e));
+}
